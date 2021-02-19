5b4f1a68838ecb2c48562bd6f4cc9d50c4c67e55
tangguoyang <etangguoyang@163.com>
2021-02-19 15:20:56 +0800

    premake

---
+++ Make/premake5.lua
@@ -0,0 +1,34 @@
+workspace "Taurus"
+    configurations { "Debug", "Release" }
+    location "Generated"
+    language "C++"
+    architecture "x86_64"
+	
+	filter "configurations:Debug"
+		symbols "On"
+	
+	filter "configurations:Release"
+		optimize "On"
+
+    filter { }
+	-- Here we use some "tokens" (the things between %{ ... }). They will be replaced by Premake
+	-- automatically when configuring the projects.
+	-- The path is relative to *this* folder
+	targetdir ("Build/Bin/%{prj.name}/%{cfg.longname}")
+    objdir ("Build/Obj/%{prj.name}/%{cfg.longname}")
+    
+project "App"
+	kind "ConsoleApp"
+	files "../Runtime/**"
+
+    includedirs "../Platform/WinPlayer/SDL2/include"
+    libdirs "../Platform/WinPlayer/SDL2/lib/x64"
+    debugenvs "PATH=%PATH%;$(ProjectDir)../../Platform/WinPlayer/SDL2/lib/x64"
+    -- runpathdirs "../Platform/WinPlayer/SDL2/lib/x64"
+    links {"SDL2.lib", "SDL2main.lib"}
+	
+	-- filter { "system:windows" }
+	-- 	links { "OpenGL32" }
+		
+	-- filter { "system:not windows" }
+	-- 	links { "GL" }
